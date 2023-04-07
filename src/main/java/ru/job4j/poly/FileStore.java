a30de7d7030ddee82e335afea3c8325894bbf724
Alex Platonov <Alexey.Platonov@magenta-technology.com>
2023-04-07 16:40:14 +0400

    1. Что такое полиморфизм. [#3819]

---
+++ src/main/java/ru/job4j/poly/FileStore.java
@@ -0,0 +1,22 @@
+package ru.job4j.poly;
+
+public class FileStore implements Store {
+    private String path;
+
+    public FileStore(String path) {
+        this.path = path;
+    }
+
+    public FileStore() {
+
+    }
+
+    public void save(String data) {
+
+    }
+
+    public String read() {
+
+        return null;
+    }
+}
