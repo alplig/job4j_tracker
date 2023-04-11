c042f6ac3f04f491f5ad5e22e32a637766fc8a8b
Alex Platonov <Alexey.Platonov@magenta-technology.com>
2023-04-11 11:24:36 +0400

    4.2. Интерфейс Input [#181092] минимальная реализация

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -1,18 +1,20 @@
 package ru.job4j.poly;
 
 public class Bus implements Transport {
+    int passengersCount = 0;
+
     @Override
     public void ride() {
-
+        System.out.println("В пути");
     }
 
     @Override
     public void passengers(int number) {
-
+        passengersCount += number;
     }
 
     @Override
     public double refuel(double fuel) {
-        return 0;
+        return fuel * 60.2;
     }
 }
