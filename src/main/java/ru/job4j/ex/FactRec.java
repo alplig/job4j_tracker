acdfc49fbb6191062d061e126b84db57d81047f2
Alex Platonov <Alexey.Platonov@magenta-technology.com>
2023-04-13 11:10:44 +0400

    0.6. Error - исключения, связанные с работой виртуальной машины [#219370]

--- src/main/java/ru/job4j/ex/FactRec.java
+++ src/main/java/ru/job4j/ex/FactRec.java
@@ -2,15 +2,15 @@ package ru.job4j.ex;
 
 public class FactRec {
     public static int calc(int n) {
-        int rsl = 1;
-        if (n != 0 && n != 1) {
-            rsl = calc(n - 1) * n;
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return calc(n - 1) * n;
         }
-        return rsl;
     }
 
     public static void main(String[] args) {
-        int rsl = calc(4);
+        int rsl = calc(3);
         System.out.println(rsl);
     }
 }
