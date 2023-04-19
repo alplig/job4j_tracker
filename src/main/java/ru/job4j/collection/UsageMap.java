0313e2b5f9540f7dc50e57752d6ec776ac31a294
Alex Platonov <Alexey.Platonov@magenta-technology.com>
2023-04-19 09:48:51 +0400

    0. Map, HashMap [#212652]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,10 @@ public class UsageMap {
         HashMap<String, String> hashMap = new HashMap<>();
         hashMap.put("mail@mail.ru", "Ivanov Ivan Ivanovich");
         hashMap.put("mail1@mail.ru", "Petrov Petr Petrovich");
+        hashMap.put("mail1@mail.ru", "Petrov Ivan Petrovich");
+        hashMap.put("mail2@mail.ru", "AAA AAA AAA");
+        hashMap.put("mail2@mail.ru", "BBB BBB BBB");
+        hashMap.put("mail2@mail.ru", "CCC CCC CCC");
         for (String key : hashMap.keySet()) {
             String value = hashMap.get(key);
             System.out.println(value);
