63d3b38b0b603e74540c02200559ba64423e1abd
Alex Platonov <Alexey.Platonov@magenta-technology.com>
2023-05-03 14:17:59 +0400

    3. Отсортировать департаменты [#34352]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,8 +5,7 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        String o1Split = o1.split("/")[0];
-        String o2Split = o2.split("/")[0];
-        return o2Split.compareTo(o1Split) != 0 ? o2Split.compareTo(o1Split) : o1.compareTo(o2);
+        int rsl = o2.split("/")[0].compareTo(o1.split("/")[0]);
+        return rsl != 0 ? rsl : o1.compareTo(o2);
     }
 }
